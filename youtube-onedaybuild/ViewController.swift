20ca320615bed7120216208366e804a426493087
jonleach16 <jonleach16@hotmail.com>
2020-06-30 10:28:28 +0100

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Jon Leach on 30/06/2020.
+//  Copyright © 2020 Jon Leach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
